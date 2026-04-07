b224dbd2707298cc521510af218692459e4b4100
Vito Secona <secona00@gmail.com>
2026-04-07 23:35:20 +0700

    add elliot-opt

---
+++ tools/elliot-opt/elliot-opt.cpp
@@ -0,0 +1,14 @@
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+#include "mlir/Support/FileUtilities.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
+int main(int argc, char **argv) {
+  mlir::registerAllPasses();
+  mlir::DialectRegistry registry;
+  mlir::registerAllDialects(registry);
+
+  return mlir::asMainReturnCode(mlir::MlirOptMain(
+      argc, argv, "Elliot Modular Optimizer Driver\n", registry));
+}
